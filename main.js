bc426b7f8e8c52571345d9200e981748f70014fe
sarvardev17 <sarvarpm7@gmail.com>
2026-04-15 17:33:28 +0500

    istalgan commit yozing

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const btnLeft = document.getElementById("btnLeft");
 const btnRight = document.getElementById("btnRight");
 const slide = document.getElementById("slide");
+const showNumber = document.getElementById("show-number");
 
 let imgs = ["./images/bmw1.jpg", "./images/bmw2.jpg", "./images/bmw3.jpg"];
 
 let active = 0;
-
+showNumber.innerHTML = active + 1; 
 btnRight.addEventListener("click", () => {
   active++;
+
+    
 
   if (active >= imgs.length) {
     active = 0;
   }
 
   slide.src = imgs[active];
+   showNumber.innerHTML = active + 1; 
 });
 
 btnLeft.addEventListener("click", () => {
@@ -24,4 +28,5 @@ btnLeft.addEventListener("click", () => {
   }
 
   slide.src = imgs[active];
+  showNumber.innerHTML = active + 1;
 });
