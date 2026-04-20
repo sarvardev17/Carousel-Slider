a01496103f778f7bdb97dde226c32d20ef69ca42
sarvardev17 <sarvarpm7@gmail.com>
2026-04-20 17:43:21 +0500

    istalgan commit yozing

--- main.js
+++ main.js
@@ -1,42 +1,10 @@
-// const btnLeft = document.getElementById("btnLeft");
-// const btnRight = document.getElementById("btnRight");
-// const slide = document.getElementById("slide");
-// const showNumber = document.getElementById("show-number");
-
-// let imgs = ["./images/bmw1.jpg", "./images/bmw2.jpg", "./images/bmw3.jpg"];
-
-
-
-// let active = 0;
-// showNumber.innerHTML = active + 1; 
-// btnRight.addEventListener("click", () => {
-//   active++;
-
-    
-
-//   if (active >= imgs.length) {
-//     active = 0;
-//   }
-
-//   slide.src = imgs[active];
-//    showNumber.innerHTML = active + 1; 
-// });
-
-// btnLeft.addEventListener("click", () => {
-//   active--;
-
-//   if (active < 0) {
-//     active = imgs.length - 1;
-//   }
-
-//   slide.src = imgs[active];
-//   showNumber.innerHTML = active + 1;
-// });
-
 const btnLeft = document.getElementById("btnLeft");
 const btnRight = document.getElementById("btnRight");
 const slide = document.getElementById("slide");
 const showNumber = document.getElementById("show-number");
+let input = document.querySelector("#input");
+let searchBtn = document.querySelector("#searchBtn");
+let text = document.querySelector("#text");
 
 let imgs = [
   "./images/bmw1.jpg",
@@ -46,10 +14,8 @@ let imgs = [
 
 let active = 0;
 
-
 slide.src = imgs[active];
 showNumber.innerHTML = active + 1;
-
 
 btnRight.addEventListener("click", () => {
   active++;
@@ -71,7 +37,6 @@ btnLeft.addEventListener("click", () => {
   updateSlider();
 });
 
-
 setInterval(() => {
   active++;
 
@@ -86,5 +51,40 @@ function updateSlider() {
   slide.src = imgs[active];
   showNumber.innerHTML = active + 1;
 }
+
+
+function showError() {
+  text.textContent = "Bunday rasm yo'q";
+  text.style.opacity = "1";
+
+  setTimeout(() => {
+    text.style.opacity = "0";
+  }, 3000);
+}
+searchBtn.addEventListener("click", () => {
+  let number = Number(input.value) - 1;
+
+  if (imgs[number]) {
+    active = number;
+    updateSlider();
+    text.style.opacity = "0";
+  } else {
+    showError();
+  }
+
+  input.value = "";
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
